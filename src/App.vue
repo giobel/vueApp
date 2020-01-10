264c1046bda9f37dd39f6963d4b2cc569d8d442e
GLOBAL\Giovanni.Brogiolo <giovanni.brogiolo>
2020-01-10 14:56:52 +1100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">    
+    <HelloWorld :msg="items"></HelloWorld>
   </div>
 </template>
 
 <script>
+
+// eslint-disable-next-line no-unused-vars
+import axios from "axios";
 import HelloWorld from './components/HelloWorld.vue'
+import { csv } from 'd3-request';
+import url from 'C:\Temp\AirtableSettings.csv';
+
+csv(url, function(err, data) {
+ console.log(data);
+})
 
 export default {
   name: 'app',
+  data(){
+    return{
+      items: [],
+    }
+  },
+      created: function () {
+    },
+  methods:{
+    
+  },//close methods
   components: {
+    // Chart
     HelloWorld
   }
-}
+  }
 </script>
 
 <style>
